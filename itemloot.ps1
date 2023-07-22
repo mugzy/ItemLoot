87ad08a30176bd05fad98d8e20d41f42dec62253
mugzy <github@photographic.org>
2023-07-22 17:48:56 -0500

    Now find sorce or destination steamid.

--- itemloot.ps1
+++ itemloot.ps1
@@ -6,13 +6,14 @@ $steamId = 1
 [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
 $steamId = [Microsoft.VisualBasic.Interaction]::InputBox("Players SteamID or 1 for all", "SteamID", "1")
 $directionSelection = [Microsoft.VisualBasic.Interaction]::InputBox("Enter All, To, or From", "Direction", "All")
-
+$p1 = '(\[\d{2}\/\d{2} \d{2}:\d{2}:\d{2}\]) T:ItemMove(.*) S:(.*) (\[.*\]) T:.* (\[-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?\])'
+$p2 = '(\[\d{2}\/\d{2} \d{2}:\d{2}:\d{2}\]) T:ItemMove(.*) S:(' + $steamId + ') (\[.*\]) T:.* (\[-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?\])'
 if ($steamId) {
     if ($steamId.length -ne 17) {
-        $datePattern = '(\[\d{2}\/\d{2} \d{2}:\d{2}:\d{2}\]) T:ItemMove(.*) S:(.*) (\[.*\]) T:.* (\[-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?\])'
+        $datePatternset = $p1
     }
     else {
-        $datePattern = '(\[\d{2}\/\d{2} \d{2}:\d{2}:\d{2}\]) T:ItemMove(.*) S:(' + $steamId + ') (\[.*\]) T:.* (\[-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?\])'
+        $datePatternset = $p2
     }
     $extraPatternFrom = 'Extra:(\d+)x (.+) (?<!moved )from (.+)'
     $extraPatternFromPicked = 'Extra:picked up (\d+)x (.*)'
@@ -26,50 +27,62 @@ if ($steamId) {
     for ($i = 0; $i -lt $lines.Length; $i++) {
         $line = $lines[$i]
         $nextLine = $lines[$i + 1]
-
-        if ($line -match $datePattern -and !$nextLine.Contains("moved from") -and ($nextLine.Contains("moved to") -or $nextLine.Contains("from") -or $nextLine.Contains("picked up") -or $nextLine.Contains("dropped"))) {
-            if ($Matches[5] -eq "[0, 0, 0]" -and ($directionSelection -eq "From" -or $directionSelection -eq "All")) {
-                $direction = "FROM"
-            }
-            elseif ($directionSelection -eq "To" -or $directionSelection -eq "All") {
-                $direction = "TO"
-            }
-            
-            $date = $Matches[1]
-            $name = $Matches[2]
-            $PlayerID = $Matches[3]
-            if ($i -lt $lines.Length - 1) {
-                
-
-                if ($direction -eq "FROM") {
-                    $pattern = $extraPatternFrom
-                    $pattern1 = $extraPatternFromPicked
+        $datePattern = $datePatternset
+        try {
+            if ($line -match $datePattern -or $nextLine.Contains($steamId)) {
+                <# Action to perform if the condition is true #>
+                if ($steamId.length -ne 1 -and $nextLine.Contains($steamId)) {
+                    $datePattern = $p1
                 }
-                elseif ($direction -eq "TO") {
-                    $pattern = $extraPatternTo
-                    $pattern1 = $extraPatternToDropped
-                }
-
-                if ($nextLine -match $pattern -or $nextLine -match $pattern1) {
-                    $container = "ground"
-                    $quantity = $Matches[1]
-                    $item = $Matches[2]
-                    $container = $Matches[3]
-                    $entry = [PSCustomObject]@{
-                        Date      = $date
-                        PlayerID  = $PlayerID
-                        Name      = $name
-                        Quantity  = $quantity
-                        Item      = $item
-                        Direction = $direction
-                        Container = if ( $Container ) { $Container } else { "ground" }
-        
+                if (($line -match $datePattern -or ($steamId.length -ne 1 -and $nextLine.Contains($steamId))) -and !$nextLine.Contains("moved from") -and ($nextLine.Contains("moved to") -or $nextLine.Contains("from") -or $nextLine.Contains("picked up") -or $nextLine.Contains("dropped"))) {
+                    if ($nextLine.Contains($steamId)) {
+                        $line -match '(\[\d{2}\/\d{2} \d{2}:\d{2}:\d{2}\]) T:ItemMove(.*) S:(.*) (\[.*\]) T:.* (\[-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?\])'
                     }
-                    $output += $entry
+                    if ($Matches[5] -eq "[0, 0, 0]" -and ($directionSelection -eq "From" -or $directionSelection -eq "All")) {
+                        $direction = "FROM"
+                    }
+                    elseif ($directionSelection -eq "To" -or $directionSelection -eq "All") {
+                        $direction = "TO"
+                    }
+                
+                    $date = $Matches[1]
+                    $name = $Matches[2]
+                    $PlayerID = $Matches[3]
+                    if ($i -lt $lines.Length - 1) {
+                    
+    
+                        if ($direction -eq "FROM") {
+                            $pattern = $extraPatternFrom
+                            $pattern1 = $extraPatternFromPicked
+                        }
+                        elseif ($direction -eq "TO") {
+                            $pattern = $extraPatternTo
+                            $pattern1 = $extraPatternToDropped
+                        }
+    
+                        if ($nextLine -match $pattern -or $nextLine -match $pattern1) {
+                            $container = "ground"
+                            $quantity = $Matches[1]
+                            $item = $Matches[2]
+                            $container = $Matches[3]
+                            $entry = [PSCustomObject]@{
+                                Date      = $date
+                                PlayerID  = $PlayerID
+                                Name      = $name
+                                Quantity  = $quantity
+                                Item      = $item
+                                Direction = $direction
+                                Container = if ( $Container ) { $Container } else { "ground" }
+            
+                            }
+                            $output += $entry
+                        }
+                    }
                 }
             }
+        } 
+        catch [System.Exception] {          
         }
-
     }
     $writefile = (Get-Item $filePath ).DirectoryName
     $date = Get-Date -format 'yyyyMMdd_HHmmss'
