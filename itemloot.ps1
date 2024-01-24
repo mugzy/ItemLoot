ec43216d64ba8fb9452ceda2d9aed1a0f47862e5
Mugzy <github@photographic.org>
2024-01-24 13:14:55 -0600

    Update to fix items pulled from broken container loot bags.

--- itemloot.ps1
+++ itemloot.ps1
@@ -17,8 +17,12 @@ $p1 = '(\[\d{2}\/\d{2} \d{2}:\d{2}:\d{2}\]) T:ItemMove(.*) S:(.*) (\[.*\]) T:.* 
 $p2 = '(\[\d{2}\/\d{2} \d{2}:\d{2}:\d{2}\]) T:ItemMove(.*) S:(' + $steamId + ') (\[.*\]) T:.* (\[-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?\])'
 $extraPatternFrom = 'Extra:(\d+)x (.+) (?<!moved )from (.+)'
 $extraPatternFromPicked = 'Extra:picked up (\d+)x (.*)'
+$extraPatternNoFrom = '^Extra:(?!.*\bfrom\b).+'
 $extraPatternTo = 'Extra:(\d+)x (.+) moved to (.+)'
 $extraPatternToDropped = 'Extra:dropped (\d+)x (.*)'
+#define regex to match "Extra:1x Burlap Shirt"
+$extraPatternUnknown = 'Extra:(\d+)x (.+)'
+
 
 $scriptpath = Get-Location
 #If the csv file does not exist, download from https://github.com/mugzy/ItemLoot/blob/main/rust_items_full.csv
@@ -50,13 +54,13 @@ if ($steamId) {
         $datePatternset = $p2
     }
     $lines = Get-Content $filePath
-
+    #remove blank lines from $lines
+    $lines = $lines | Where-Object { $_ -ne "" }
+    
     for ($i = 0; $i -lt $lines.Length; $i++) {
-
-
-
         $line = $lines[$i]
         $nextLine = $lines[$i + 1]
+        $nextLine3 = $lines[$i + 3]
         $datePattern = $datePatternset
         try {
             if ($line -match $datePattern -or $nextLine.Contains($steamId)) {
@@ -64,13 +68,16 @@ if ($steamId) {
                 if ($steamId.length -ne 1 -and $nextLine.Contains($steamId)) {
                     $datePattern = $p1
                 }
-                if (($line -match $datePattern -or ($steamId.length -ne 1 -and $nextLine.Contains($steamId))) -and !$nextLine.Contains("moved from") -and ($nextLine.Contains("moved to") -or $nextLine.Contains("from") -or $nextLine.Contains("picked up") -or $nextLine.Contains("dropped"))) {
+                if (($line -match $datePattern -or ($steamId.length -ne 1 -and $nextLine.Contains($steamId))) -and !$nextLine.Contains("moved from") ) {
 
                     if ($Matches[5] -eq "[0, 0, 0]" -and ($directionSelection -eq "From" -or $directionSelection -eq "All") -and !$nextLine.Contains("dropped")) {
                         $direction = "FROM"
                     }
                     elseif ($directionSelection -eq "To" -or $directionSelection -eq "All") {
                         $direction = "TO"
+                    }
+                    elseif ($Matches[5] -eq "[0, 0, 0]" -and ($directionSelection -eq "All") -and $nextLine -notmatch "(?i)(dropped|moved|from|to)"){
+                        $direction = "unknown"
                     }
                 
                     $date = $Matches[1]
@@ -82,14 +89,20 @@ if ($steamId) {
                         if ($direction -eq "FROM") {
                             $pattern = $extraPatternFrom
                             $pattern1 = $extraPatternFromPicked
+                            $pattern3 = $extraPatternNoFrom
                         }
                         elseif ($direction -eq "TO") {
                             $pattern = $extraPatternTo
                             $pattern1 = $extraPatternToDropped
                         }
+                        elseif ($direction -eq "unknown") {
+                            $pattern = $extraPatternUnknown
+                        }
     
-                        if ($nextLine -match $pattern -or $nextLine -match $pattern1) {
-
+                        if ($nextLine -match $pattern -or $nextLine -match $pattern1 -or ($nextLine -match $pattern3 -and !$nextLine3.Contains("from"))) {
+                            if (!$nextLine.Contains("from")) {
+                                if($nextLine -match $extraPatternUnknown){}
+                            }
                             $OuterLoopProgressParameters = @{
                                 Activity         = 'Parsing File'
                                 Status           = 'Progress->'
@@ -98,7 +111,7 @@ if ($steamId) {
                             }
                             Write-Progress @OuterLoopProgressParameters
                             
-                            $container = "ground"
+                            $container = "unknown"
                             $quantity = $Matches[1]
                             $item = $Matches[2]
                             $shortname = Get-RustItemID -ItemName $item
@@ -111,7 +124,7 @@ if ($steamId) {
                                 Item      = $item.Replace('?','-')
                                 Direction = $direction
                                 Command = "inventory.give $shortname $quantity"
-                                Container = if ( $Container ) { $Container } else { "ground" }
+                                Container = if ( $Container ) { $Container } else { "unknown" }
                             }
                             
                             $output += $entry
